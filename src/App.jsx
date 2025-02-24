441f465702bb7834e50809ee53b5bc6bf8bcb9eb
Jhon <jhonpulido762@gmail.com>
2025-02-24 14:45:49 -0500

    ft: Configuración de rutas ✅

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import './App.css'
+import AppRouter from './routes/AppRouter';
 
 function App() {
 
   return (
-    <div className="bg-amber-200 my-20">
-      <h1 className='text-3xl text-center'>DESARROLLO FRONTEND</h1>
-    </div>
+    <AppRouter/>
   )
 }
 
---
+++ src/pages/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='w-full h-full min-h-screen bg-amber-500'>
+      
+    </div>
+  )
+}
+
+export default Login

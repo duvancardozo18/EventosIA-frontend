dd41836c5f432e862514b4033ff2b4156be528c9
Jhon <jhonpulido762@gmail.com>
2025-02-23 23:00:33 -0500

    Inicio del Frontend

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="bg-amber-200 my-20">
+      <h1 className='text-3xl text-center'>DESARROLLO FRONTEND</h1>
+    </div>
+  )
+}
+
+export default App

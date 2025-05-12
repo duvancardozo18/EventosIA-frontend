a7d26c360a6d519d6149190b85c04fac88dafd9a
f3lipe22 <andresfelipediaz782@gmail.com>
2025-05-12 00:30:42 -0500

    feat: corregir errores posteriores

--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -20,27 +20,18 @@ import Layout from "./layout";
   //import ParticipantsEvent from "../components/screens/events/eventscreate/form/ParticipantsEvent";
   //import FeedingEvent from "../components/screens/events/eventscreate/form/FeedingEvent";
 
+  // Componentes de creación de eventos
+  import CardsView from "../screens/events/CardsView";
+  import Event from "../screens/events/createEvent/tabs/Event";
+  import TypeEvent from "../screens/events/createEvent/tabs/TypeEvent";
+  import LocationEvent from "../screens/events/createEvent/tabs/LocationEvent";
+  import CreateEvent from "../screens/events/createEvent/CreateEvent";
 
-//Paginas
-import Login from "../pages/auth/Login";
-import Homepage from "../pages/Homepage";
-import VerifyAccount from "../pages/auth/VerifyAccount";
-import Register from "../pages/auth/Register";
-import ResetPassword from "../pages/forgotpassword/ResetPassword";
-import ResetPasswordForm from "../pages/forgotpassword/ResetPasswordForm";
-
-// Componentes de creación de eventos
-import CardsView from "../screens/events/CardsView";
-import Event from "../screens/events/createEvent/tabs/Event";
-import TypeEvent from "../screens/events/createEvent/tabs/TypeEvent";
-import LocationEvent from "../screens/events/createEvent/tabs/LocationEvent";
-import CreateEvent from "../screens/events/createEvent/CreateEvent";
-
-// Componentes de edición de eventos
-import EditEvent from "../screens/events/editEvent/EditEvent";
-import EditEventTab from "../screens/events/editEvent/tab/Event";
-import EditTypeEventTab from "../screens/events/editEvent/tab/TypeEvent";
-import EditLocationEventTab from "../screens/events/editEvent/tab/LocationEvent";
+  // Componentes de edición de eventos
+  import EditEvent from "../screens/events/editEvent/EditEvent";
+  import EditEventTab from "../screens/events/editEvent/tab/Event";
+  import EditTypeEventTab from "../screens/events/editEvent/tab/TypeEvent";
+  import EditLocationEventTab from "../screens/events/editEvent/tab/LocationEvent";
 
   //Paginas Dashboard
   import HomeDashboard from "../pages/dashboard/home";
@@ -49,7 +40,7 @@ import EditLocationEventTab from "../screens/events/editEvent/tab/LocationEvent"
   import EventDetail from "../pages/events/EventDetail";
   import EventBilling from "../pages/billing/EventBilling";
   import PaymentView from "../pages/billing/PaymentView";
-  
+
   const router = createBrowserRouter([
     {
       path: "/",
@@ -64,23 +55,15 @@ import EditLocationEventTab from "../screens/events/editEvent/tab/LocationEvent"
       element: <Register />,
     },
     {
-      path: "/verify-email/:token",
+      path: "/verify-account",
       element: <VerifyAccount />,
-    },
-    {
-      path: "/verification-success",
-      element: <VerificationSuccess />,
-    },
-    {
-      path: "/verification-redirect",
-      element: <VerificationRedirect />,
     },
     {
       path: "/reset-password",
       element: <ResetPassword />,
     },
     {
-      path: "/reset-password-form/:token",
+      path: "/reset-password-form",
       element: <ResetPasswordForm />,
     },
     {
@@ -91,125 +74,68 @@ import EditLocationEventTab from "../screens/events/editEvent/tab/LocationEvent"
       path: "/dashboard",
       element: <PrivateRoute element={<Layout />} />,
       children: [
-
-
-//Paginas Dashboard
-import HomeDashboard from "../pages/dashboard/home";
-import Reports from "../pages/dashboard/Reports";
-import NotificationsPage from "../pages/notifications/NotificationsPage";
-import EventDetail from "../pages/events/EventDetail";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Homepage />,
-  },
-  {
-    path: "/login",
-    element: <Login />,
-  },
-  {
-    path: "/register",
-    element: <Register />,
-  },
-  {
-    path: "/verify-account",
-    element: <VerifyAccount />,
-  },
-  {
-    path: "/reset-password",
-    element: <ResetPassword />,
-  },
-  {
-    path: "/reset-password-form",
-    element: <ResetPasswordForm />,
-  },
-  {
-    path: "/notifications",
-    element: <PrivateRoute element={<NotificationsPage />} />,
-  },  
-  {
-    path: "/dashboard",
-    element: <PrivateRoute element={<Layout />} />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="inicio" replace />,
-      },
-      {
-        path: "inicio",
-        element: <HomeDashboard />,
-      },
-      {
-        path: "events",
-        element: <CardsView />,
-      },
-      {
-        path: "reports",
-        element: <PrivateRoute element={<Reports />} />,
-      },
-      {
-        path: "help",
-        element: <h1 className="title">Ayuda</h1>,
-      },      
-      {
-        path: "events/create-event",
-        element: <PrivateRoute element={<CreateEvent />} />,
-        children: [
-          { index: true, element: <Navigate to="evento" replace /> },
-          { path: "evento", element: <Event /> },
-          { path: "tipoEvento", element: <TypeEvent /> },
-          { path: "ubicacion", element: <LocationEvent /> },
-        ],
-      },
-      // Nueva sección para la edición de eventos
-      {
-        path: "events/edit-event",
-        element: <PrivateRoute element={<EditEvent />} />,
-        children: [
-          { index: true, element: <Navigate to="editarEvento" replace /> },
-          { path: "editarEvento", element: <EditEventTab /> },
-          { path: "editarTipoEvento", element: <EditTypeEventTab /> },
-          { path: "editarUbicacion", element: <EditLocationEventTab /> },
-        ],
-      },
-      {
-        path: "events/detail-events/:id",
-        element: <PrivateRoute element={<EventDetail />} />,
-      },
-    ],
-  },
-]);
-
-
-function AppRouter() {
-  return (
-    <AuthProvider>
-      <RouterProvider router={router} />
-    </AuthProvider>
-  );
-}
-
-export default AppRouter;
-
+        {
+          index: true,
+          element: <Navigate to="inicio" replace />,
         },
         {
-          
-          path: "events/billing-event/:id",
-          element: <PrivateRoute element={<EventBilling />} />,
-
+          path: "inicio",
+          element: <HomeDashboard />,
         },
         {
-          
-          path: "events/billing-event/:id/payment-view",
-          element: <PrivateRoute element={<PaymentView />} />,
-
+          path: "events",
+          element: <CardsView />,
         },
-        
+        {
+          path: "reports",
+          element: <PrivateRoute element={<Reports />} />,
+        },
+        {
+          path: "help",
+          element: <h1 className="title">Ayuda</h1>,
+        },      
+        {
+          path: "events/create-event",
+          element: <PrivateRoute element={<CreateEvent />} />,
+          children: [
+            { index: true, element: <Navigate to="evento" replace /> },
+            { path: "evento", element: <Event /> },
+            { path: "tipoEvento", element: <TypeEvent /> },
+            { path: "ubicacion", element: <LocationEvent /> },
+          ],
+        },
+        // Nueva sección para la edición de eventos
+        {
+          path: "events/edit-event",
+          element: <PrivateRoute element={<EditEvent />} />,
+          children: [
+            { index: true, element: <Navigate to="editarEvento" replace /> },
+            { path: "editarEvento", element: <EditEventTab /> },
+            { path: "editarTipoEvento", element: <EditTypeEventTab /> },
+            { path: "editarUbicacion", element: <EditLocationEventTab /> },
+          ],
+        },
+        {
+          path: "events/detail-events/:id",
+          element: <PrivateRoute element={<EventDetail />} />,
+        },
+
+                {
+            
+            path: "events/billing-event/:id",
+            element: <PrivateRoute element={<EventBilling />} />,
+
+          },
+          {
+            
+            path: "events/billing-event/:id/payment-view",
+            element: <PrivateRoute element={<PaymentView />} />,
+
+          },
       ],
     },
   ]);
-  
+
   function AppRouter() {
     return (
       <AuthProvider>
